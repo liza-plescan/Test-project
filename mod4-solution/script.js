69f1516d5b4d39504d0848a104394a72c7e2b0af
liza-plescan <liza.plescan@gmail.com>
2018-06-24 18:10:00 +0100

    remove extra name

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -38,7 +38,7 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Pit", "Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
